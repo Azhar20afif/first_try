07911b4139780cea48ce05b4e273455ccfe25800
Azhar20afif <124029366+Azhar20afif@users.noreply.github.com>
2023-01-31 13:26:48 +0500

    Create first.py

---
+++ first.py
@@ -0,0 +1,18 @@
+def calculate(num1, operator, num2):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        return "Invalid operator"
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+result = calculate(num1, operator, num2)
+print(result)
